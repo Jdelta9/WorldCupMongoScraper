7265109afae913249ba1ad53a2a8203adb8398ae
Jessica Cervantes <J1123C@gmail.com>
2018-08-28 22:30:11 -0700

    modify code

--- models/note.js
+++ models/note.js
@@ -1,26 +1,25 @@
-// Require Mongoose
-const mongoose = require('mongoose');
-
-// Create a Schema Class
-const Schema = mongoose.Schema;
-
-// Create Comment Schema
-const CommentSchema = new Schema({
-
-    // Author's Name
-    author: {
-        type: String
+// Note model
+// ==========
+// Require mongoose
+var mongoose = require("mongoose");
+// Create the schema class using mongoose's schema method
+var Schema = mongoose.Schema;
+// Create the noteSchema with the schema object
+var noteSchema = new Schema({
+    // The headline is the article associate with the note
+    _headlineId: {
+        type: Schema.Types.ObjectId,
+        ref: "Headline"
     },
-    // Comment Content
-    content: {
-        type: String
-    }
-
+    // date is just a string
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    // as is the noteText
+    noteText: String
 });
-
-
-// Create the Comment model with Mongoose
-const Comment = mongoose.model('Comment', CommentSchema);
-
-// Export the Model
-module.exports = Comment;
+// Create the Note model using the noteSchema
+var Note = mongoose.model("Note", noteSchema);
+// Export the Note model
+module.exports = Note;
